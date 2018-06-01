5f51c45198f8dbbf40a2fdd59fa98443384eb4ce
anlaakso <an.laaakso@gmail.com>
2018-06-01 14:00:09 +0300

    Fix quotes

--- browser.service/src/main.js
+++ browser.service/src/main.js
@@ -18,7 +18,7 @@ async function browse({ url }) {
 async function process(message) {
 	const data = JSON.parse(message);
 	const result = await browse(data);
-	return JSON.stringify({ html: result, ...data });
+	return JSON.stringify({ html: `\"${result}\"`, ...data });
 }
 
 async function run() {
